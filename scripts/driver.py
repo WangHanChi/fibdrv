aaf3f8936959781893bbedfe52c3521dc3284d26
WangHanChi <hank1122334455667788990@gmail.com>
2023-03-15 03:27:01 +0800

    Add a script to draw the plot The script will run the series in 1 to 500 and get the calculate time, then plot in the plot which can show its efficacy.

---
+++ scripts/driver.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+
+runs = 500
+
+
+def data_processing(data_set, n):
+    list = []
+    for i in range(100):
+        data_set[0][i].sort()
+        for j in range(1, 7):
+            data_set[0][i][j] = 0
+            data_set[0][i][n - j] = 0
+        list.append(sum(data_set[0][i]) / len(data_set[0][i]-6))
+    return list
+
+if __name__ == "__main__":
+    Ys = []
+    comp_porc = subprocess.run('cd ..', shell = True)
+    for i in range(runs):
+        comp_porc = subprocess.run('sudo taskset 0xb ./client', shell = True)
+    output = np.loadtxt('data.txt', dtype = 'float').T
+    Ys.append(output)
+    # print(Ys[0][3])
+    Y = data_processing(Ys, runs)
+    X = list(range(1, 101))
+
+    fig, ax = plt.subplots(1, 1, sharey = True)
+    ax.set_title('perf', fontsize = 16)
+    ax.set_xlabel(r'$n_{th}$ fibonacci', fontsize = 16)
+    ax.set_ylabel('time (ns)', fontsize = 16)
+    ax.plot(X, Y, marker = '+', markersize = 7, label = 'kernel')
+    ax.legend(loc = 'upper left')
+    plt.savefig('plot/plot.png') 
+    plt.show()
+    comp_porc = subprocess.run('rm -rf data.txt', shell = True)
